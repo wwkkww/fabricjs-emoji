d40cb43639ebfbe91893c846cbe685f2cd6ee54f
Kevin <wongkokwah@gmail.com>
2019-06-03 23:11:41 +0800

    implemented fadeout image, svg, text

--- src/Canvas.js
+++ src/Canvas.js
@@ -3,6 +3,9 @@ import 'emoji-mart/css/emoji-mart.css'
 import { fabric } from 'fabric';
 import { Smile } from 'react-feather';
 import { Picker, Emoji } from 'emoji-mart';
+import thumbsUp from './images/thumbs_up.png'
+import like_256 from './images/like_256.png'
+import like_svg from './images/like.svg'
 
 
 class Canvas extends Component {
@@ -19,6 +22,34 @@ class Canvas extends Component {
 
   componentDidMount() {
     this.canvas = new fabric.Canvas('c', { backgroundColor: 'yellow' })
+    this.setCanvasEventListener()
+  }
+
+  setCanvasEventListener() {
+    this.canvas.on(
+      "object:added", () => {
+        this.updateModifications('added')
+      })
+
+    this.canvas.on(
+      "object:modified", () => {
+        this.updateModifications('modified')
+      })
+
+    this.canvas.on(
+      "object:removed", () => {
+        this.updateModifications('removed')
+      })
+
+    this.canvas.on(
+      "after:render", () => {
+        this.updateModifications('after render')
+      })
+  }
+
+  updateModifications(event) {
+    console.log(event)
+
   }
 
   onMessageChanged = event => {
@@ -33,7 +64,7 @@ class Canvas extends Component {
       const textObject = new fabric.IText('Enter text here...', textOptions);
       textObject.text = this.state.message
       this.updateCanvas(textObject)
-      this.setState({message : ''})
+      this.setState({ message: '' })
       console.log('send message')
     }
   }
@@ -58,6 +89,16 @@ class Canvas extends Component {
     this.textRef.current.focus()
   }
 
+  addEmoji2 = (emoji) => {
+    console.log('add emoji', emoji)
+
+    const text = `${emoji.native}`;
+    const textObject = new fabric.IText(text, textOptions);
+    this.updateCanvas(textObject)
+    this.setState({ showEmojiPicker: false });
+    // this.textRef.current.focus()
+  }
+
   fadeOutObject = () => {
 
     // const circle = new fabric.Circle({ radius: 20, fill: 'red', left: 100, top: 100 });
@@ -74,7 +115,7 @@ class Canvas extends Component {
     activeObj.set('opacity', 1)
 
     activeObj.animate('opacity', '0', {
-      duration: 3000,
+      duration: 1500,
       // onChange: this.canvas.renderAll(),
       onChange: () => this.canvas.requestRenderAll(),
       onComplete: () => {
@@ -83,6 +124,82 @@ class Canvas extends Component {
         });
         // this.canvas.remove(activeObj);
       }
+    });
+  }
+
+  fadeOutImage = () => {
+    fabric.Image.fromURL(like_256, (img) => {
+      img.set({
+        // width: 600,
+        // height: 338,
+        left: 100,
+        top: 100,
+        hasBorders: false,
+        hasControls: false
+      });
+      this.canvas.add(img);
+      this.canvas.setActiveObject(img);
+
+      let activeObj = this.canvas.getActiveObject();
+
+      // activeObj.setOpacity(1);
+      activeObj.set('opacity', 1)
+
+      activeObj.animate('opacity', '0', {
+        duration: 1500,
+        // onChange: this.canvas.renderAll(),
+        onChange: () => this.canvas.requestRenderAll(),
+        onComplete: () => {
+          this.canvas.getActiveObjects().forEach((obj) => {
+            this.canvas.remove(obj)
+          });
+          // this.canvas.remove(activeObj);
+        }
+      });
+    });
+  }
+
+  fadeOutSVG = () => {
+    fabric.loadSVGFromURL(like_svg, (objects, options) => {
+      const svg = fabric.util.groupSVGElements(objects, options);
+      svg.left = 200;
+      svg.top = 150;
+      svg.scaleToWidth(180);
+      svg.scaleToHeight(180);
+      svg.hasBorders = false;
+      svg.hasControls = false;
+      this.canvas.add(svg);
+      // let group = [];
+      // const loadedObjects = new fabric.Group(group);
+
+      // loadedObjects.set({
+      //   left: 100,
+      //   top: 100,
+      //   width: 175,
+      //   height: 175
+      // });
+
+      // this.canvas.add(loadedObjects);
+
+      this.canvas.setActiveObject(svg);
+
+      let activeObj = this.canvas.getActiveObject();
+
+      // activeObj.setOpacity(1);
+      activeObj.set('opacity', 1)
+
+      activeObj.animate('opacity', '0', {
+        duration: 1500,
+        // onChange: this.canvas.renderAll(),
+        onChange: () => this.canvas.requestRenderAll(),
+        onComplete: () => {
+          this.canvas.getActiveObjects().forEach((obj) => {
+            this.canvas.remove(obj)
+          });
+          // this.canvas.remove(activeObj);
+        }
+      });
+      // this.canvas.renderAll();
     });
   }
 
@@ -100,7 +217,7 @@ class Canvas extends Component {
             this.state.showEmojiPicker ?
               (
                 <Picker
-                  onSelect={this.addEmoji}
+                  onSelect={this.addEmoji2}
                   showPreview={false}
                   showSkinTones={false}
                   native={true}
@@ -109,7 +226,7 @@ class Canvas extends Component {
               ) : null
           }
         </div>
-        <div style={{ border: 'all', borderRadius: '5px', borderColor: '#bsbsbs', borderWidth: '2px'}}>
+        <div style={{ border: 'all', borderRadius: '5px', borderColor: '#bsbsbs', borderWidth: '2px' }}>
           <textarea
             ref={this.textRef}
             className=""
@@ -122,7 +239,6 @@ class Canvas extends Component {
           <span>
             <button
               type="button"
-              className="toggle-emoji"
               onClick={this.toggleEmojiPicker}
             >
               <Smile />
@@ -130,14 +246,27 @@ class Canvas extends Component {
 
             <button
               type="button"
-              className="toggle-emoji"
               onClick={this.fadeOutObject}
             >
               Fade
             </button>
+
+            <button
+              type="button"
+              onClick={this.fadeOutImage}
+            >
+              Image
+            </button>
+
+            <button
+              type="button"
+              onClick={this.fadeOutSVG}
+            >
+              SVG
+            </button>
           </span>
         </div>
-        
+
       </div>
     );
   }
@@ -147,11 +276,11 @@ export default Canvas;
 
 const textOptions = {
   fontFamily: 'Helvetica',
-  fontSize: 40,
+  fontSize: 150,
   fontWeight: 'bold',
   fill: 'blue',
-  left: 20,
-  top: 20,
+  left: 300,
+  top: 100,
   radius: 10,
   borderRadius: '25px',
   hasRotatingPoint: true
@@ -161,10 +290,11 @@ const praiseOptions = {
   fontFamily: 'Helvetica',
   fontSize: 250,
   fontWeight: 'bold',
+  hasBorders: false,
+  hasControls: false,
   fill: 'blue',
   left: 100,
   top: 100,
   radius: 10,
-  borderRadius: '25px',
-  hasRotatingPoint: true
+  borderRadius: '25px'
 };
